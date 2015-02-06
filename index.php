572ddd025de2611781d7c7b50548045e857ecc1b
Tech No Freaky <varunsridharan23@gmail.com>
2015-02-06 19:25:07 +0530

    Base Commit
    Base Commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<script src="jquery.js" type="text/javascript"></script>
+<script src="responsive_image.js" type="text/javascript"></script>
+<script>
+$(document).ready(function(){
+	
+	$('img.rsimg').rsimage({siteURL : 'http://10.0.1.9/tools/rsimg/process.php'});
+});
+//(options: exact, portrait, landscape, auto, crop)
+</script>
+
+ 
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_1.jpg"  height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_2.jpg"  height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_3.jpg"  height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_4.jpg"  height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_5.jpg"  height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_6.jpg"  height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_7.jpg"  height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_8.jpg"  height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_9.jpg"  height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_10.jpg" height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_11.jpg" height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_12.jpg" height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_13.jpg" height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_14.jpg" height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_15.jpg" height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_16.jpg" height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_17.jpg" height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_18.jpg" height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_19.jpg" height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_21.jpg" height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_22.jpg" height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_23.jpg" height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_24.jpg" height="" class="rsimg"  />
+<img data-src="http://10.0.1.9/iir/sites/ab/site/trunk/html2/ab gallery/ab_gallery_25.jpg" height="" class="rsimg"  />
